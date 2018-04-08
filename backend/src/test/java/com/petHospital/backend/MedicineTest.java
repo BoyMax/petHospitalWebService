c5c3c21f3582daa6bbe24039d5955f966040fbaa
李琪琦 <445072942@qq.com>
2018-04-08 22:17:12 +0800

    add unit test for Medicine

--- backend/src/test/java/com/petHospital/backend/MedicineTest.java
+++ backend/src/test/java/com/petHospital/backend/MedicineTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.transaction.Transactional;
@@ -19,14 +20,17 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.petHospital.backend.controller.MedicineController;
 import com.petHospital.backend.dto.MedicineDTO;
 import com.petHospital.backend.dto.ResponseDTO;
+import com.petHospital.backend.model.Illness;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MedicineTest 
 {
-	String existId = "3";
+	String existId = "1";
 	String notExistId = "2";
+	Long illnessId = 1L;
+	Long notExistedIllnessId = 100L;
 	
 	@Autowired
     private MedicineController medicineController;
@@ -69,8 +73,21 @@ public class MedicineTest
     		medicineDTO.setPrice(88.9);
     		medicineDTO.setProductionDate(Date.valueOf("2018-01-01"));
     		medicineDTO.setExpirationDate(Date.valueOf("2018-12-31"));
+    		List<Illness> illnesses = new ArrayList<Illness>();
+    		Illness illness = new Illness();
+    		illness.setId(illnessId);
+    		illnesses.add(illness);
+    		medicineDTO.setIllnesses(illnesses);
     		ResponseEntity<ResponseDTO<MedicineDTO>> responseEntity = medicineController.editMedicine(medicineDTO);
         assertTrue( responseEntity.getBody().getStatus() == "success" );
+        
+        //notExistedIllnessId
+		illness.setId(notExistedIllnessId);
+		illnesses.clear();
+		illnesses.add(illness);
+		medicineDTO.setIllnesses(illnesses);
+		ResponseEntity<ResponseDTO<MedicineDTO>> responseEntity2 = medicineController.editMedicine(medicineDTO);
+		assertTrue( responseEntity2.getBody().getStatus() == "success" );
     }
     
     @Test
@@ -83,6 +100,11 @@ public class MedicineTest
 		medicineDTO.setPrice(88.9);
 		medicineDTO.setProductionDate(Date.valueOf("2018-01-01"));
 		medicineDTO.setExpirationDate(Date.valueOf("2018-12-31"));
+		List<Illness> illnesses = new ArrayList<Illness>();
+		Illness illness = new Illness();
+		illness.setId(illnessId);
+		illnesses.add(illness);
+		medicineDTO.setIllnesses(illnesses);
 		ResponseEntity<ResponseDTO<MedicineDTO>> responseEntity = medicineController.addMedicine(medicineDTO);
 		assertTrue( responseEntity.getBody().getStatus() == "success" );
     }
@@ -93,5 +115,8 @@ public class MedicineTest
     public void testDeleteMedicine() {
 		ResponseEntity<ResponseDTO<MedicineDTO>> responseEntity = medicineController.deleteMedicine(existId);
 		assertTrue( responseEntity.getBody().getStatus() == "success" );
+		
+		ResponseEntity<ResponseDTO<MedicineDTO>> responseEntity2 = medicineController.deleteMedicine(notExistId);
+		assertTrue( responseEntity2.getBody().getStatus() == "failed" );
     }
 }
