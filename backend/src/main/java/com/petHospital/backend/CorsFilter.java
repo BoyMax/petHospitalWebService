7f7949099f3bbedf843b22714c94b6eef23d5507
李琪琦 <445072942@qq.com>
2018-04-08 10:05:23 +0800

    Access-Control-Allow-Headers

--- backend/src/main/java/com/petHospital/backend/CorsFilter.java
+++ backend/src/main/java/com/petHospital/backend/CorsFilter.java
@@ -28,7 +28,7 @@ public class CorsFilter implements Filter {
         response.setHeader("Access-Control-Allow-Credentials", "true");  
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT,OPTIONS, DELETE");  
         response.setHeader("Access-Control-Max-Age", "3600");  
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");  
+        response.setHeader("Access-Control-Allow-Headers", "*");  
         chain.doFilter(req, res);  
     }  
     public void init(FilterConfig filterConfig) {}  
