f42e8c663b65d2a1b7997793d4de93b357f39b27
李琪琦 <445072942@qq.com>
2018-04-08 10:17:12 +0800

    Access-Control-Allow-Headers configuration

--- backend/src/main/java/com/petHospital/backend/CorsFilter.java
+++ backend/src/main/java/com/petHospital/backend/CorsFilter.java
@@ -12,25 +12,29 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Component;
 
-@Component  
-public class CorsFilter implements Filter {  
-  
-    final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CorsFilter.class);  
-  
-  
-  
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {  
-        HttpServletResponse response = (HttpServletResponse) res;  
-  
-        //HttpServletRequest reqs = (HttpServletRequest) req;  
-  
-        response.setHeader("Access-Control-Allow-Origin","*");  
-        response.setHeader("Access-Control-Allow-Credentials", "true");  
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT,OPTIONS, DELETE");  
-        response.setHeader("Access-Control-Max-Age", "3600");  
-        response.setHeader("Access-Control-Allow-Headers", "*");  
-        chain.doFilter(req, res);  
-    }  
-    public void init(FilterConfig filterConfig) {}  
-    public void destroy() {}  
-} 
+@Component
+public class CorsFilter implements Filter {
+
+	final static org.slf4j.Logger logger = org.slf4j.LoggerFactory.getLogger(CorsFilter.class);
+
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletResponse response = (HttpServletResponse) res;
+
+		// HttpServletRequest reqs = (HttpServletRequest) req;
+
+		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT,OPTIONS, DELETE");
+		response.setHeader("Access-Control-Max-Age", "3600");
+		response.setHeader("Access-Control-Allow-Headers",
+				"Origin, No-Cache, X-Requested-With, If-Modified-Since, Pragma, Last-Modified, Cache-Control, Expires, Content-Type, X-E4M-With，Authorization，api_key");
+		chain.doFilter(req, res);
+	}
+
+	public void init(FilterConfig filterConfig) {
+	}
+
+	public void destroy() {
+	}
+}
